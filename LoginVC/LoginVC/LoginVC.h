d339d935f55169eeb1138aac2e0ffb8e1821373c
jeni Kakadiya <jeni.arvaan@gmail.com>
2025-02-11 15:10:44 +0530

    add login

---
+++ LoginVC/LoginVC/LoginVC.h
@@ -0,0 +1,18 @@
+//
+//  LoginVC.h
+//  LoginVC
+//
+//  Created by Arvaan Jeni on 11/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginVC.
+FOUNDATION_EXPORT double LoginVCVersionNumber;
+
+//! Project version string for LoginVC.
+FOUNDATION_EXPORT const unsigned char LoginVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginVC/PublicHeader.h>
+
+
